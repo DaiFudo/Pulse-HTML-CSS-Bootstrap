488fae4bd2a9ffe5ebe8994fbff580d1c428076a
Fudo <dai.fudo@mail.ru>
2020-03-31 15:34:38 +0300

    Back JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $(".carousel__inner").slick({
-    speed: 1300,
+    speed: 1200,
     prevArrow:
       '<button type = "button"class="slick-prev"><img src=../icons/left.svg ></button>',
     nextArrow:
